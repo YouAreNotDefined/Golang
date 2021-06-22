70525c76846b57e542b665e90124ba05095af4e1
YouAreNotDefined <for.1self.ik@gmail.com>
2021-06-22 23:07:26 +0900

    ファイルリスト表示と静的ファイルのサーバー作成

--- app/main.go
+++ app/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	fmt.Println("Hello golang from docker!")
+	// display file list
+	err := filepath.Walk("root", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("path: %#v\n", path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// server listening
+	port := "8080"
+	http.Handle("/", http.FileServer(http.Dir("root/")))
+	log.Printf("Server listening on http://localhost:%s/", port)
+	log.Print(http.ListenAndServe(":"+port, nil))
 }
